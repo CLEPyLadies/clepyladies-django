ba6e3a6e55474a856a89d5f4f96d0898e3feeca4
mutterberg <Marissa@UtterbergDataDev.com>
2020-02-15 15:27:57 -0500

    Add functional tests skeleton.

---
+++ functional_tests/tests.py
@@ -0,0 +1,23 @@
+import os
+import unittest
+
+from selenium import webdriver
+
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_django_installed(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Django', self.browser.title)
+
+if __name__ == "__main__":
+    unittest.main(warnings='ignore')
