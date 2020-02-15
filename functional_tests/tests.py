86267fef40d9f4a3be101f1ed6bb6e12107a287c
Marissa <Marissa@UtterbergDataDev.com>
2020-02-15 16:43:29 -0500

    Update django.yml (#10)
    * Update django.yml
    
    * Update django.yml
    
    Per https://github.com/ianwalter/bff-webdriver/blob/master/.github/workflows/ci.yml#L16 from https://ianwalter.dev/running-selenium-webdriver-tests-using-github-actions/
    
    * Update tests.py
    
    * Update tests.py
    
    * Update tests.py
    
    * Update django.yml
    
    Remove container strategy
    
    * Update django.yml
    
    * Update django.yml

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -7,7 +7,8 @@ from selenium import webdriver
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
-        self.browser = webdriver.Chrome()
+        self.browser = webdriver.Firefox()
+        self.test_host = os.environ.get('TEST_HOST')
         staging_server = os.environ.get('STAGING_SERVER')
         if staging_server:
             self.live_server_url = 'http://' + staging_server
@@ -16,7 +17,10 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_django_installed(self):
-        self.browser.get('http://localhost:8000')
+        if self.test_host:
+            self.browser.get(self.test_host)
+        else:
+            self.browser.get('http://localhost:8000')
         self.assertIn('Django', self.browser.title)
 
 if __name__ == "__main__":
