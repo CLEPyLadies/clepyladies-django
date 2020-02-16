eca900d85e9cb5f423034938e569ac4418b0b40f
mutterberg <Marissa@UtterbergDataDev.com>
2020-02-16 16:35:40 -0500

    Initial check-in functionality via admin interface

--- pyladies/admin.py
+++ pyladies/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from pyladies.models import Attendance, Event, Profile
+from pyladies.models import CheckIn, Event, EventInstance, Profile
 
 # Register your models here.
-admin.site.register(Attendance)
+admin.site.register(CheckIn)
 admin.site.register(Event)
+admin.site.register(EventInstance)
 admin.site.register(Profile)
